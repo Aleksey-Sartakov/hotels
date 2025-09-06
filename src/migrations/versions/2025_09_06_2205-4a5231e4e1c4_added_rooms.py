e590e3752397e9431f4928ccb8b0b307d64120a3
Aleksey.Sartakov <sartakov.aleksey.d@gmail.com>
2025-09-06 22:09:19 +0700

    настроено форматирование для файлов миграций

---
+++ src/migrations/versions/2025_09_06_2205-4a5231e4e1c4_added_rooms.py
@@ -0,0 +1,40 @@
+"""added rooms
+
+Revision ID: 4a5231e4e1c4
+Revises: df9ad8f9fa73
+Create Date: 2025-09-06 22:05:31.042310
+
+"""
+
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = "4a5231e4e1c4"
+down_revision: Union[str, Sequence[str], None] = "df9ad8f9fa73"
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.create_table(
+        "rooms",
+        sa.Column("id", sa.Integer(), nullable=False),
+        sa.Column("hotel_id", sa.Integer(), nullable=False),
+        sa.Column("title", sa.String(), nullable=False),
+        sa.Column("description", sa.String(), nullable=True),
+        sa.Column("price", sa.Integer(), nullable=False),
+        sa.Column("quantity", sa.Integer(), nullable=False),
+        sa.ForeignKeyConstraint(
+            ["hotel_id"],
+            ["hotels.id"],
+        ),
+        sa.PrimaryKeyConstraint("id"),
+    )
+
+
+def downgrade() -> None:
+    op.drop_table("rooms")
